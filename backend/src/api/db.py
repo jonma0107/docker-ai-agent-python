bc5475a668ad3fab83b11f3016bd5edcc1e315a8
Jonathan Meza <jduplantier44@gmail.com>
2025-06-30 03:32:39 -0500

    Update for database models

--- backend/src/api/db.py
+++ backend/src/api/db.py
@@ -5,10 +5,10 @@ from sqlmodel import Session, SQLModel
 
 DATABASE_URL = os.environ.get("DATABASE_URL")
 
-if DATABASE_URL == "":
-    raise NotImplementedError("`DATABASE_URL` needs to be set.")
+if not DATABASE_URL:
+    raise NotImplementedError("`DATABASE_URL` is not set.")
 
-# DATABASE_URL = DATABASE_URL.replace("postgres://", "postgres+psycopg://")
+DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql+psycopg://")
 
 engine = sqlmodel.create_engine(DATABASE_URL)
 
